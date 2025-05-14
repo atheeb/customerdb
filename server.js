f281cd578850e003299c951162e55cfd5d8b1725
atheeb <atheeb412@gmail.com>
2025-05-14 13:48:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(bodyParser.json());
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'customer_records'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
